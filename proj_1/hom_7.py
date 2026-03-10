8ccee903e3cf0d59bdc2af452dd84698b8ffae76
dhyeong03 <gd101805@gmail.com>
2026-03-10 17:23:19 +0900

    Update hom_7.py

--- proj_1/hom_7.py
+++ proj_1/hom_7.py
@@ -1,37 +1,22 @@
 # Pro7(미래도시)
-
 INF = int(1e9)
+n,m = map(int, input().split())
 
-n, m = map(int, input().split())
+graph = [ [INF]*(n+1) for _ in range(n+1)]
 
-graph = [[INF] * (n+1) for _ in range(n+1)]
+for _ in range(m) :
+  com1, com2 = map(int,input().split())
+  graph[com1][com2] = 1 
+  graph[com2][com1] = 1 
 
-for i in range(1, n+1):
-    graph[i][i] = 0
+x,k = map(int,input().split())
 
-for _ in range(m):
+for middle in range(1,n+1) :
+  for start in range(1,n+1) :
+    for end in range(1,n+1) :
+      graph[start][end] = min(graph[start][end], graph[start][middle]+graph[middle][end])
 
-    a, b = map(int, input().split())
+result = graph[1][k] + graph[k][x] 
 
-    graph[a][b] = 1
-    graph[b][a] = 1
-
-
-x, k = map(int, input().split())
-
-for i in range(1, n+1):
-
-    for a in range(1, n+1):
-
-        for b in range(1, n+1):
-
-            graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b])
-
-
-distance = graph[1][k] + graph[k][x]
-
-if distance >= INF:
-    print(-1)
-
-else:
-    print(distance)
+if result >= INF : print(-1)
+else : print(result)
